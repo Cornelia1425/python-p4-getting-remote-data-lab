0398dff386ba0f51e1dbe28b7ec1437c0c3a874e
yiranshu <shuyiran1425@hotmail.com>
2024-05-29 13:24:21 -0400

    content

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -6,8 +6,20 @@ class GetRequester:
     def __init__(self, url):
         self.url = url
 
+    # def get_response_body(self):
+    #     response=requests.get(self.url)
+    #     return response.text
+
+    # def load_json(self):
+    #     response_body = self.get_response_body()
+    #     json_data = json.loads(response_body)
+    #     return json_data
+    
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        json_data = json.loads(response_body)
+        return json_data
